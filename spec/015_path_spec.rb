8f6e9bff2c33bcd1b166c2d41d962a950f7178bd
Nathaniel Miller <dukeoflaser@gmail.com>
2016-03-04 23:58:14 +0000

    Add tests for path.rb

--- spec/015_path_spec.rb
+++ spec/015_path_spec.rb
@@ -24,3 +24,38 @@ require_relative "../lib/path.rb" # Code your solution here.
 # Test the negative and positive results, for example,
 # when describing absolute_path? make sure to test
 # the results when sent a relative and absolute path.
+
+describe Path do
+  let(:ab_path) {Path.new("/abs/o/lute/path.rb")}
+  let(:rel_path) {Path.new("rel/a/tive/path.rb")}
+  
+  describe "#normalize_path" do
+    it "should return an absolute file path if given an absolute path" do
+      expect(ab_path.normalize_path).to eq("/abs/o/lute/path.rb")
+    end
+    
+    it "should return an absolute file path if given a relative path" do
+      expect(rel_path.normalize_path).to eq("#{Dir.pwd}/rel/a/tive/path.rb")
+    end
+  end
+  
+  describe "#absolute_path?" do
+    it "should return true if given an absolute path" do
+      expect(ab_path.absolute_path?).to eq(true)
+    end
+    
+    it "should return false if given a relative path" do
+      expect(rel_path.absolute_path?).to eq(false)
+    end  
+  end
+  
+  describe "#relative_path?" do
+    it "should return false if given an absolute path" do
+      expect(ab_path.relative_path?).to eq(false)
+    end
+    
+    it "should return true if given a relative path" do
+      expect(rel_path.relative_path?).to eq(true)
+    end    
+  end
+end
